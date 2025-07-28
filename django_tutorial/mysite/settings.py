022e681160710590c563bd1d563dfb81a42a6f67
jrdostal <dostal325@gmail.com>
2025-07-27 22:56:16 -0400

    updated based on deprecated module

--- django_tutorial/mysite/settings.py
+++ django_tutorial/mysite/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     "wagtail.wagtailsearch",
     "wagtail.wagtailredirects",
     "wagtail.wagtailforms",
+    "wagtail_modeladmin",
     'compressor',
     "modelcluster",
     "taggit",
