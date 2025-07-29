9330fcc4eda448bf1cc2a934c3afeb9203109465
jrdostal <dostal325@gmail.com>
2025-07-28 21:44:15 -0400

    fixed sql db

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,6 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import PostForm, CommentForm
 
-
-
-
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
     return render(request, "blog/post_list.html", {"posts": posts})
