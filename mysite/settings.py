80a196254b498a55dd897f53b9736a5a603f025e
jrdostal <dostal325@gmail.com>
2025-07-28 21:00:52 -0400

    css updates

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -23,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-&&!nk+uf%5w(+2kxs9z493$l0qy*=416o-mh=pip)jgt1=j04^"
 
 
-# SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = False
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost","127.0.0.1",".pythonanywhere.com"]
 
@@ -149,10 +148,10 @@ WAGTAIL_SITE_NAME = "John Dostal\'s Blog"
 
 
 STATIC_ROOT = BASE_DIR / 'static'
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 #Don't add a trailing slash to Django-served URLs
 APPEND_SLASH = False
